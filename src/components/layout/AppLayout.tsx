2bd30902c26d11389edc56fc5e0115f952b9ab89
Tavido77 <148545453+Tavido77@users.noreply.github.com>
2026-02-19 18:03:58 +0000

    update

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -16,9 +16,9 @@ export function AppLayout({ children }: AppLayoutProps) {
   }));
 
   return (
-    <div className="flex h-screen bg-background">
-      <Sidebar workspaces={sidebarWorkspaces} />
-      <main className="flex-1 overflow-auto">
+    <div className="flex h-screen bg-background flex-col md:flex-row">
+      <Sidebar workspaces={sidebarWorkspaces} className="w-full md:w-1/4" />
+      <main className="flex-1 overflow-auto p-4 md:p-6">
         {children}
       </main>
     </div>
